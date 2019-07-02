2a2a40a2b3e3f04e4fb97ffd56a4f180e859c7c1
peterwangpf <wangpf2@163.com>
2019-07-02 17:41:47 +0800

    商品折扣信息

---
+++ src/main/java/com/coding/sales/entity/SaleTicket.java
@@ -0,0 +1,16 @@
+package com.coding.sales.entity;
+/*
+折扣券
+ */
+public class SaleTicket {
+    private double saleType;
+    private String saleContent;
+
+    public double getSaleType() {
+        return saleType;
+    }
+
+    public void setSaleType(double saleType) {
+        this.saleType = saleType;
+    }
+}
