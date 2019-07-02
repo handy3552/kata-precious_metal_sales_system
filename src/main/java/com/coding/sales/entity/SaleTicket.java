5daceea6cf46d79624fe81c1ad593c1ab7b99ee1
peterwangpf <wangpf2@163.com>
2019-07-02 18:22:06 +0800

    商品信息类，折扣信息类修改

--- src/main/java/com/coding/sales/entity/SaleTicket.java
+++ src/main/java/com/coding/sales/entity/SaleTicket.java
@@ -1,8 +1,12 @@
 package com.coding.sales.entity;
+
 /*
-折扣券
+客户支付时如果使用打折券，对订单中的 所有 可打折贵金属将享受优惠：
+95折券：如果商品参与95折，则应收金额为95折
+9折券：如果商品参与9折，则应收金额为9折
  */
 public class SaleTicket {
+
     private double saleType;
     private String saleContent;
 
