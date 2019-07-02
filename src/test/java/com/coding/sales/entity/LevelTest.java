c98289cd5c41257fceb311813f6dcf091fbc4af2
handy3552 <handi3@163.com>
2019-07-02 17:56:46 +0800

    commit test; 等级测试类

---
+++ src/test/java/com/coding/sales/entity/LevelTest.java
@@ -0,0 +1,12 @@
+package com.coding.sales.entity;
+
+import org.junit.Test;
+
+public class LevelTest {
+
+    @Test
+    public void getLevel() {
+        Level level = new Level();
+        System.out.println(level.getLevel("1").getLevel_name());
+    }
+}
